578ed9b4dc315dd5a634d12c1637d2f34dad395b
BorRotar <bor.rotar1@hotmail.com>
2022-07-22 20:04:26 +0200

    Added Context for Cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,23 @@
 import styles from "./HeaderCartButton.module.css";
 import CartIcon from '../Cart/CartIcon.js';
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
+
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount
+  }, 0);
+
   return (
-    <button onClick={props.onClick} className={styles.button}>
+          <button onClick={props.onClick} className={styles.button}>
       <span className={styles.icon}>
        <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={styles.badge}>3</span>
+      <span className={styles.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
